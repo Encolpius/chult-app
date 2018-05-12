2cf3c87c7c6f60386b318e25a579e7dc03b9a2d7
Andy Hunt <andy.l.hunt85@gmail.com>
2018-05-12 18:20:22 -0400

    add welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def welcome
+    "Hello Mina, Zach, Liz, Grant, Dan, and Jeff! Welcome to Chult..."
+  end
+
 end
