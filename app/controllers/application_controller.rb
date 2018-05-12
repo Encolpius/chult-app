effba60851d2281243fd27d1d4c8d19c572c5786
Andy Hunt <andy.l.hunt85@gmail.com>
2018-05-12 18:28:50 -0400

    update welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def welcome
-    "Hello Mina, Zach, Liz, Grant, Dan, and Jeff! Welcome to Chult..."
+    render html: "Hello Mina, Zach, Liz, Grant, Dan, and Jeff! Welcome to Chult..."
   end
 
 end
